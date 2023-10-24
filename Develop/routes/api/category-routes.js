49192c78524b57f3ca8d006eb91d79cc4815dfee
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-10-24 16:59:46 -0400

    adds all routes for categories

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -42,24 +42,69 @@ router.get("/:id", async (req, res) => {
     }
   } catch (err) {
     console.error(err);
-    res
-      .status(500)
-      .json({
-        error: "Unable to retrieve the category and its associated products.",
-      });
+    res.status(500).json({
+      error: "Unable to retrieve the category and its associated products.",
+    });
   }
 });
 
-router.post("/", (req, res) => {
-  // create a new category
+router.post("/", async (req, res) => {
+  try {
+    // Extract category data from the request body
+    const { category_name } = req.body;
+
+    // Create a new category in the database
+    const newCategory = await Category.create({ category_name });
+
+    res.status(201).json(newCategory);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Unable to create a new category." });
+  }
 });
 
-router.put("/:id", (req, res) => {
-  // update a category by its `id` value
+router.put("/:id", async (req, res) => {
+  try {
+    const categoryId = req.params.id;
+    const { category_name } = req.body;
+
+    // Find the category by its `id` value
+    const category = await Category.findByPk(categoryId);
+
+    if (!category) {
+      return res.status(404).json({ error: "Category not found" });
+    }
+
+    // Update the category's data
+    category.category_name = category_name;
+    await category.save();
+
+    res.status(200).json(category);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Unable to update the category." });
+  }
 });
 
-router.delete("/:id", (req, res) => {
-  // delete a category by its `id` value
+router.delete("/:id", async (req, res) => {
+  try {
+    const categoryId = req.params.id;
+
+    // Find the category by its `id` value
+    const category = await Category.findByPk(categoryId);
+
+    if (!category) {
+      return res.status(404).json({ error: "Category not found" });
+    }
+
+    // Delete the category
+    await category.destroy();
+
+    res.status(204).send(); // Send a 204 No Content response indicating successful deletion
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Unable to delete the category." });
+  }
 });
 
 module.exports = router;
