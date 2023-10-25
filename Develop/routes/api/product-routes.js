e092e345218e07d1b2af05c5815f0a218e617518
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-10-24 22:42:45 -0400

    finishes routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -69,11 +69,7 @@ router.post("/", async (req, res) => {
     const { product_name, price, stock, tagIds } = req.body;
 
     // Create the product in the database
-    const newProduct = await Product.create({
-      product_name,
-      price,
-      stock,
-    });
+    const newProduct = await Product.create();
 
     if (tagIds && tagIds.length) {
       // If tagIds are provided, associate the product with tags
@@ -89,27 +85,6 @@ router.post("/", async (req, res) => {
     res.status(500).json({ error: "Unable to create a new product." });
   }
 });
-
-Product.create(req.body)
-  .then((product) => {
-    // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-    if (req.body.tagIds.length) {
-      const productTagIdArr = req.body.tagIds.map((tag_id) => {
-        return {
-          product_id: product.id,
-          tag_id,
-        };
-      });
-      return ProductTag.bulkCreate(productTagIdArr);
-    }
-    // if no product tags, just respond
-    res.status(200).json(product);
-  })
-  .then((productTagIds) => res.status(200).json(productTagIds))
-  .catch((err) => {
-    console.log(err);
-    res.status(400).json(err);
-  });
 
 // update product
 router.put("/:id", (req, res) => {
